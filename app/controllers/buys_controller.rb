e9a839150412b85db984f194d71ca8b1f45e10ce
Bonky564 <nk-trans@i.softbank.jp>
2021-09-04 18:38:32 +0900

    formオブジェクト

---
+++ app/controllers/buys_controller.rb
@@ -0,0 +1,27 @@
+class BuysController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @buy_address = BuyAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def buy_params
+    params.require(:buy_address).permit(:postal_code, :prefecture_id, :city, :house_num, :building_name, :telephone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
