b5892b80d02f96c692fa654d135ce6e557b78754
Bonky564 <nk-trans@i.softbank.jp>
2021-09-05 23:02:31 +0900

    購入機能

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,10 +1,11 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_num, :building_name, :telephone_num, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_num, :building_name, :telephone_num, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :city
     validates :house_num
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
